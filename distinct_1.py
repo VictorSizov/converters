fb6f43c7c520f901c06fc7ac0405cbe67795fca3
VictorSizov <victor.sizov@gmail.com>
2020-02-03 20:25:42 +0300

    DEV-335 <span class="head"> <span class="note">  => <noindex> DEV-335 склейка noindex DEV-335 копирование атрибутов SPEECH

--- distinct_1.py
+++ distinct_1.py
@@ -1,11 +1,41 @@
 # -*- Encoding: utf-8 -*-
-from converter_basic import ConverterBasic,fill_arg_for_converter
+from converter_basic import ConverterBasic, fill_arg_for_converter
+from lxml import etree
 from lxml_ext import LxmlExt
 
 
 class Distinct1(ConverterBasic):
 
-    def check_distinct_separ(self, text, before_distinct):
+    @staticmethod
+    def is_space(text):
+        return text is None or text == '' or text.isspace()
+
+    @staticmethod
+    def fill_speech(tree):
+        attrs = None
+        for speech in tree.getroot().iter('speech'):
+            if speech.attrib:
+                attrs = speech.attrib
+            elif attrs is not None:
+                speech.attrib.update(attrs)
+
+    def join_noindex(self, tree):
+        for noindex in tree.getroot().iter('noindex', 'span'):
+            if noindex.tag == 'span' and noindex.attrib.get("class", "") in ("note", "head"):
+                noindex.tag = 'noindex'
+                noindex.attrib.clear()
+                if not self.is_space(noindex.text):
+                    noindex.text = '[' + noindex.text + ']'
+            prev = noindex.getprevious()
+            if prev is None or prev.tag is etree.PI or prev.tag is etree.Comment or prev.tag != 'noindex':
+                continue
+            if self.is_space(prev.tail):
+                txt = LxmlExt.concat_text(prev.text, prev.tail)
+                noindex.text = LxmlExt.concat_text(txt, noindex.text)
+                prev.getparent().remove(prev)
+
+    @staticmethod
+    def check_distinct_separ(text, before_distinct):
         if text is None or text == '':
             return " "
         if isinstance(text, str):
@@ -27,6 +57,8 @@ class Distinct1(ConverterBasic):
             else:
                 prev.tail = self.check_distinct_separ(prev.tail, True)
             distinct.tail = self.check_distinct_separ(distinct.tail, False)
+        self.join_noindex(tree)
+        self.fill_speech(tree)
 
 
 if __name__ == '__main__':
